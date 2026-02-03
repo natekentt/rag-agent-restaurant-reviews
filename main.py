4f827dd29f98132465a93d044d707e185d0c9296
Nathaniel Kent <59068197+natekentt@users.noreply.github.com>
2026-02-02 20:29:09 -0800

    set up terminal input

--- main.py
+++ main.py
@@ -14,16 +14,13 @@ Here is the question to answer: {question}
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
-result = chain.invoke(
-    {
-        "reviews": [],
-        "question": "what is the best pizza place in town?"
-    }
-)
+while True:
+    print("\n\n--------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
 
-print(result)
-# result:
-# Based on my expertise, I'd be happy to help!
-# Unfortunately, I don't have any specific reviews from customers at this time. However, I can suggest that you check out some of the top-rated pizzerias in your area by looking up online review platforms such as Yelp or Google Reviews.
-# That being said, I can give you a general answer about what makes a great pizza place. A good pizza restaurant should have a variety of toppings, crispy crust, and a flavorful sauce. They should also have a welcoming atmosphere and attentive service to make your dining experience enjoyable.
-# If you're looking for recommendations, I can suggest asking friends or colleagues who have tried pizzerias in the area for their opinions. Alternatively, you could try checking out online review sites to see which pizza place has received the highest ratings from customers.
+    if question == "q":
+        break
+
+    result = chain.invoke({"reviews": [], "question": question})
+    print(result)
