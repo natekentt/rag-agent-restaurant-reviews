a0f68ed98dac7415bdd062b7d4ac8d4936424885
Nathaniel Kent <59068197+natekentt@users.noreply.github.com>
2026-02-03 09:12:30 -0800

    Add vector DB/embeddings

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2")
 
@@ -22,5 +23,7 @@ while True:
     if question == "q":
         break
 
-    result = chain.invoke({"reviews": [], "question": question})
+    # finds top k reviews based on similarity search
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
     print(result)
---
+++ vector.py
@@ -0,0 +1,40 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd
+
+df = pd.read_csv("realistic_restaurant_reviews.csv")
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+
+db_location = "./chrome_langchain_db"
+add_documents = not os.path.exists(db_location)
+
+if add_documents:
+    # need both lists
+    documents = []
+    ids = []
+
+    for i, row in df.iterrows():
+        document = Document(
+            page_content=row["Title"] + " " + row["Review"],
+            metadata={"rating": row["Rating"], "date": row["Date"]},
+            id=str(i)
+        )
+        ids.append(str(i))
+        documents.append(document)
+
+vector_store = Chroma(
+    collection_name="restaurant_reviews",
+    persist_directory=db_location,
+    embedding_function=embeddings
+)
+
+# adds embeddings to the vector store
+if add_documents:
+    vector_store.add_documents(documents=documents, ids=ids)
+
+# make vector store resuable by LLM
+retriever = vector_store.as_retriever(
+    search_kwargs={"k": 5}  # gets back 5 reviews
+)
